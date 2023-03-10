10d9f5f6ca7bc0c3118ea7919887e3898cdf4ab0
João Marcelo Cunha <77308533+Jooaomar@users.noreply.github.com>
2023-03-09 21:41:57 -0300

    Update main.ts

--- front-end/src/main.ts
+++ front-end/src/main.ts
@@ -1,10 +1,50 @@
-// const jsonResponse = await fetch("http://localhost:8000/tarefas");
-// const jsonData = await jsonResponse.json();
-// console.log(jsonData);
+import axios from 'axios';
+
+async function getTasks() {
+  const response = await axios.get('http://localhost:8000/tarefas');
+  return response.data;
+}
 
 
-const textResponse = await fetch("http://localhost:8000/tarefas");
-const textData = await textResponse.text();
-document.getElementById("tarefas-div").innerHTML = textData;
+const tasks = await getTasks();
+
+const taskListElement = document.querySelector('#task-list');
+
+//  Buscando dados
+tasks.forEach(task => {
+  const taskElement = document.createElement('li');
+  taskElement.textContent = task.descricao;
+  taskListElement.appendChild(taskElement);
+});
 
 
+// Enviando dados
+
+const form = document.getElementById('create-task-form');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const taskDescription = document.getElementById('task-description').value;
+  const responsavel = document.getElementById('task-responsavel').value;
+  const taskPriority = document.getElementById('task-priority').value;
+  const taskSituacao = document.getElementById('task-situacao').value;
+  const taskNivel = document.getElementById('task-nivel').value;
+  const taskData = {
+    id: 0,
+    responsavel: responsavel,
+    descricao: taskDescription,
+    nivel: taskNivel,
+    situacao: taskSituacao,
+    prioridade: taskPriority,
+  };
+
+  axios.post('http://localhost:8000/adicionar/', taskData)
+    .then((response) => {
+      console.log('Tarefa criada com sucesso:', response.data);
+      // Aqui você pode redirecionar para a página de lista de tarefas ou atualizar a lista na mesma página
+    })
+    .catch((error) => {
+      console.error('Erro ao criar tarefa:', error);
+    });
+});
